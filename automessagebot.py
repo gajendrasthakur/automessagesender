6ea38b926db4e3934236f835757e19403e245818
GAJENDRA SINGH THAKUR <71806431+gajendra1405@users.noreply.github.com>
2020-12-28 22:00:36 +0530

    Update automessagebot.py

--- automessagebot.py
+++ automessagebot.py
@@ -11,6 +11,6 @@ pywhatkit.sendwhatmsg('+919907355128','Hi There',20,38)
 # Hi There should be replaced with your message you want to send to your loved ones.
 # 20 should be replaced with the hour in 24 hour clock on which time you want to send message.
 # 38 should be replaced with the minute on which time you want to send your message.
-# For example in my case +919907355128 is the number on which I wanna send my WhatsApp Message,Hi There is the message I want to send,20 i.e 8 PM in 12 hour clock is the 
-Hour time and 38 is the minute.
+# For example in my case +919907355128 is the number on which I wanna send my WhatsApp Message,Hi There is the message I want to send,20 i.e 8 PM in 12 hour clock is the Hour time and 38 is the minute.
 # This should be used only for good purposes.
+#web.whatsapp.com should be login to your destop from your WhatsApp Account you want to send your Message.
