a7fd0102c72ea25999307e7420b74946bd8311bd
GAJENDRA SINGH THAKUR <71806431+gajendrasthakur@users.noreply.github.com>
2022-06-23 22:39:23 +0530

    Update automessagebot.py

--- automessagebot.py
+++ automessagebot.py
@@ -5,12 +5,12 @@ Author: Gajendra Singh Thakur
 
 #import the necessary module!
 import pywhatkit
-pywhatkit.sendwhatmsg('+919907355128','Hi There',20,38)
+pywhatkit.sendwhatmsg('+9199073xxxxx','Hi There',20,38)
 
-# +919907355128 should be replaced with the number you want to send WHATSAPP MESSAGE with country code.
+# +9199073xxxxx should be replaced with the number you want to send WHATSAPP MESSAGE with country code.
 # Hi There should be replaced with your message you want to send to your loved ones.
 # 20 should be replaced with the hour in 24 hour clock on which time you want to send message.
 # 38 should be replaced with the minute on which time you want to send your message.
-# For example in my case +919907355128 is the number on which I wanna send my WhatsApp Message,Hi There is the message I want to send,20 i.e 8 PM in 12 hour clock is the Hour time and 38 is the minute.
+# For example in my case +9199073xxxx is the number on which I wanna send my WhatsApp Message,Hi There is the message I want to send,20 i.e 8 PM in 12 hour clock is the Hour time and 38 is the minute.
 # This should be used only for good purposes.
 #web.whatsapp.com should be login to your destop from your WhatsApp Account you want to send your Message.
